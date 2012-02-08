c07a9beb0636d8606b0ea9d80da7d9de651e6ee1
ammaraskar <ammar_askar@hotmail.com>
2012-02-09 00:13:58 +0500

    YOU SUCK AMMAR

--- src/to/joe/j2mc/reports/J2MC_Reports.java
+++ src/to/joe/j2mc/reports/J2MC_Reports.java
@@ -2,11 +2,19 @@ package to.joe.j2mc.reports;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import to.joe.j2mc.reports.command.ReportCommand;
+import to.joe.j2mc.reports.command.ReportHandlingCommand;
+
 public class J2MC_Reports extends JavaPlugin{
 
 	public ReportsManager Manager;
 	public void onEnable(){
 		this.Manager = new ReportsManager(this);
+		Manager.LoadDataIntially();
+		
+		this.getCommand("report").setExecutor(new ReportCommand(this));
+		this.getCommand("r").setExecutor(new ReportHandlingCommand(this));
+		
 		this.getLogger().info("Reports module enabled");
 	}
 	
